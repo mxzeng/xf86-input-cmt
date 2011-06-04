69baa17291b6048bdec24ada7256bb65aba2121b
Daniel Kurtz <djkurtz@chromium.org>
2011-06-03 18:31:22 -0700

    Add and install cmt-properties.h
    Properties definition file just has property definition strings.
    These aren't yet wired to real xinput properties yet.
    
    BUG=chromium-os:16097
    TEST=None yet.
    
    Change-Id: I2f26549fb24b0cccd441967ba71bffb41f7786d4

---
+++ include/cmt-properties.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef _CMT_PROPERTIES_H_
+#define _CMT_PROPERTIES_H_
+
+/**
+ * Descriptions of properties exported by the driver.
+ */
+
+/* 8 bit (BOOL) */
+#define CMT_PROP_TAPTOCLICK "Enable Tap-To-Click"
+
+/* 32 bit */
+#define CMT_PROP_MOTION_SPEED "Motion Speed"
+
+/* 32 bit, 2 values, vertical, horizontal */
+#define CMT_PROP_SCROLL_SPEED "Scroll Speed"
+
+#endif
